a598f53a422d6a65761ccfa180ce6cfdecc8fc8e
Thibault HUTTIN-PASSERON <thibault.passeron@swhawk.eu>
2025-01-04 11:02:33 +0100

    [1] Chore: renamed occurences of Game Template in various files

--- rules/src/KitsuSetup.ts
+++ rules/src/KitsuSetup.ts
@@ -1,6 +1,6 @@
 import { MaterialGameSetup } from '@gamepark/rules-api'
-import { GameTemplateOptions } from './GameTemplateOptions'
-import { GameTemplateRules } from './GameTemplateRules'
+import { KitsuOptions } from './KitsuOptions'
+import { KitsuRules } from './KitsuRules'
 import { LocationType } from './material/LocationType'
 import { MaterialType } from './material/MaterialType'
 import { PlayerColor } from './PlayerColor'
@@ -9,10 +9,10 @@ import { RuleId } from './rules/RuleId'
 /**
  * This class creates a new Game based on the game options
  */
-export class GameTemplateSetup extends MaterialGameSetup<PlayerColor, MaterialType, LocationType, GameTemplateOptions> {
-  Rules = GameTemplateRules
+export class KitsuSetup extends MaterialGameSetup<PlayerColor, MaterialType, LocationType, KitsuOptions> {
+  Rules = KitsuRules
 
-  setupMaterial(_options: GameTemplateOptions) {
+  setupMaterial(_options: KitsuOptions) {
   }
 
   start() {
