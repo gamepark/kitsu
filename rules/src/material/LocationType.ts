6b63d32709e01b9f3b56361ec3200a1d9c8b8d17
Mathieu Guerry <mathieu.guerry@hotmail.fr>
2025-01-06 13:17:44 +0100

    [4] feat: Identify and create locations

--- rules/src/material/LocationType.ts
+++ rules/src/material/LocationType.ts
@@ -1,2 +1,20 @@
 export enum LocationType {
+    WisdomBoardZone = 1,
+    KitsunePawnArea,
+    PowerTokenArea,
+    StackCardsArea,
+    DiscardedCardsArea,
+    DiscardedPowerTokenArea,
+    SetupCardZone,
+    ClanCardZone,
+    LeaderTokenSpot,
+    PowerTokenSpot,
+    PlayerHandZone,
+    PlayedCardZone
 }
+
+/*
+Zone: placed directly on table
+Spot: placed on cards
+Area: placed on WisdomBoard
+*/
