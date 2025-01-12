9dab005d4da7942bf491fd85168f16ab054cb09f
Thibault HUTTIN-PASSERON <thibault.passeron@swhawk.eu>
2025-01-12 02:03:00 +0100

    [11] Tests: created tests for 6 players setup

--- rules/tests/KitsuSetup.spec.ts
+++ rules/tests/KitsuSetup.spec.ts
@@ -202,4 +202,136 @@ describe('Setup tests', () => {
             expect(powerTokens.every(powerToken => powerToken.location.type === LocationType.PowerTokenSpotOnWisdomBoard && powerToken.id === powerToken.location.id)).toBe(true);
         });
     });
+
+    describe('Given a 6-player setup', () => {
+        test.each([
+            {options: {players: [
+                        {team: TeamColor.Zenko, id: 3},
+                        {team: TeamColor.Yako, id: 1},
+                        {team: TeamColor.Yako, id: 4},
+                        {team: TeamColor.Yako, id: 2},
+                        {team: TeamColor.Zenko, id: 5},
+                        {team: TeamColor.Zenko, id: 6}
+                    ]}, expected: [3, 1, 5, 4, 6, 2]},
+            {options: {players: [
+                        {team: TeamColor.Zenko, id: 5},
+                        {team: TeamColor.Zenko, id: 1},
+                        {team: TeamColor.Yako, id: 3},
+                        {team: TeamColor.Yako, id: 2},
+                        {team: TeamColor.Zenko, id: 4},
+                        {team: TeamColor.Yako, id: 6}
+                    ]}, expected: [5, 3, 1, 2, 4, 6]},
+            {options: {players: [
+                        {team: TeamColor.Zenko, id: 1},
+                        {team: TeamColor.Zenko, id: 4},
+                        {team: TeamColor.Zenko, id: 3},
+                        {team: TeamColor.Yako, id: 6},
+                        {team: TeamColor.Yako, id: 2},
+                        {team: TeamColor.Yako, id: 5}
+                    ]}, expected: [1, 6, 4, 2, 3, 5]},
+            {options: {players: [
+                        {team: TeamColor.Zenko, id: 2},
+                        {team: TeamColor.Yako, id: 5},
+                        {team: TeamColor.Zenko, id: 3},
+                        {team: TeamColor.Yako, id: 1},
+                        {team: TeamColor.Zenko, id: 6},
+                        {team: TeamColor.Yako, id: 4}
+                    ]}, expected: [2, 5, 3, 1, 6, 4]},
+            {options: {players: [
+                        {team: TeamColor.Yako, id: 3},
+                        {team: TeamColor.Yako, id: 1},
+                        {team: TeamColor.Zenko, id: 4},
+                        {team: TeamColor.Yako, id: 2},
+                        {team: TeamColor.Zenko, id: 5},
+                        {team: TeamColor.Zenko, id: 6}
+                    ]}, expected: [3, 4, 1, 5, 2, 6]},
+            {options: {players: [
+                        {team: TeamColor.Zenko, id: 3},
+                        {team: TeamColor.Yako, id: 1},
+                        {team: TeamColor.Zenko, id: 4},
+                        {team: TeamColor.Yako, id: 2},
+                        {team: TeamColor.Yako, id: 5},
+                        {team: TeamColor.Zenko, id: 6}
+                    ]}, expected: [3, 1, 4, 2, 6, 5]},
+        ])('KistuSetup.setupNewMaterial({players: [$options.players.0, $options.players.1, $options.players.2, $options.players.3, $options.players.4, $options.players.5}) should reorder the players according to teams : $expected', ({options, expected}) => {
+            // Given
+            const setup = new KitsuSetup();
+
+            // When
+            setup.setupMaterial(options);
+
+            // Then
+            expect(setup.players).toHaveLength(2);
+            expect(setup.players).toEqual(expected);
+        });
+
+        test('KistuSetup.setupNewMaterial() should reorder the players according to teams', () => {
+            // Given
+            const setup = new KitsuSetup();
+            const options = {
+                players : [
+                    {team: TeamColor.Zenko, id: 3},
+                    {team: TeamColor.Yako, id: 1},
+                    {team: TeamColor.Yako, id: 4},
+                    {team: TeamColor.Yako, id: 2},
+                    {team: TeamColor.Zenko, id: 5},
+                    {team: TeamColor.Zenko, id: 6}
+                ]
+            };
+
+            // When
+            setup.setupMaterial(options);
+
+            // Then
+            expect(setup.players).toEqual([3, 1, 5, 4, 6, 2]);
+        });
+
+        test('KitsuSetup.setupMaterial() should create the 30 Kitsu cards in the Deck location', () => {
+            // Given
+            const setup = new KitsuSetup();
+            const options = {
+                players : [
+                    {team: TeamColor.Zenko, id: 3},
+                    {team: TeamColor.Yako, id: 1},
+                    {team: TeamColor.Yako, id: 4},
+                    {team: TeamColor.Yako, id: 2},
+                    {team: TeamColor.Zenko, id: 5},
+                    {team: TeamColor.Zenko, id: 6}
+                ]
+            };
+
+            // When
+            setup.setupMaterial(options);
+            const kitsuCardItems = setup.material(MaterialType.KitsuCard).getItems();
+            const kitsuCardIds = kitsuCardItems.map(kitsuCard => kitsuCard.id);
+
+            // Then
+            expect(kitsuCardItems).toHaveLength(24);
+            expect(kitsuCardItems.every(item => item.location.type === LocationType.KitsuCardDeckSpotOnWisdomBoard)).toBe(true);
+            expect(kitsuCardIds).toContainEqual(getEnumValues(KitsuCard));
+        });
+
+        test('KitsuSetup.setupMaterial() should create the 5 power tokens and place them in the corresponding spots', () => {
+            // Given
+            const setup = new KitsuSetup();
+            const options = {
+                players : [
+                    {team: TeamColor.Zenko, id: 3},
+                    {team: TeamColor.Yako, id: 1},
+                    {team: TeamColor.Yako, id: 4},
+                    {team: TeamColor.Yako, id: 2},
+                    {team: TeamColor.Zenko, id: 5},
+                    {team: TeamColor.Zenko, id: 6}
+                ]
+            };
+
+            // When
+            setup.setupMaterial(options);
+            const powerTokens = setup.material(MaterialType.PowerToken).getItems();
+
+            // Then
+            expect(powerTokens).toHaveLength(5);
+            expect(powerTokens.every(powerToken => powerToken.location.type === LocationType.PowerTokenSpotOnWisdomBoard && powerToken.id === powerToken.location.id)).toBe(true);
+        });
+    });
 });
