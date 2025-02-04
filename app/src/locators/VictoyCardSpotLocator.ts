e20954b4dbb6e13f9e5a6483091cfc0dbe26dacf
Mathieu Guerry <mathieu.guerry@hotmail.fr>
2025-02-04 23:05:27 +0100

    [17] Fix: Integrated review suggestion

--- app/src/locators/VictoyCardSpotLocator.ts
+++ app/src/locators/VictoyCardSpotLocator.ts
@@ -1,9 +1,7 @@
 import { PileLocator } from "@gamepark/react-game";
-import { MaterialType } from "@gamepark/kitsu/material/MaterialType";
 
 class VictoryCardLocator extends PileLocator {
-    parentItemType = MaterialType.WisdomBoard
-    coordinates = { x: 0, y: -9.45 }
+    coordinates = { x: -60, y: 0 }
     limit = 2
     radius = 90
     maxAngle = 90
