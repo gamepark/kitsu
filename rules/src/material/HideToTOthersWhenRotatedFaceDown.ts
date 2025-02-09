b013920c1d27c5a3c4f260dc25d44516679d1aea
Thibault HUTTIN-PASSERON <thibault.passeron@swhawk.eu>
2025-02-09 22:31:03 +0100

    [20] Feat: implemented HideToOthersWhenRotatedFaceDown hiding strategy

--- rules/src/material/HideToTOthersWhenRotatedFaceDown.ts
+++ rules/src/material/HideToTOthersWhenRotatedFaceDown.ts
@@ -1,4 +1,11 @@
 import { HidingSecretsStrategy, MaterialItem } from '@gamepark/rules-api';
+import { KitsuCardRotation } from './KitsuCardRotation';
 import { LocationType } from './LocationType';
 
-export const hideToTOthersWhenRotatedFaceDown: HidingSecretsStrategy<number, LocationType> = (item: MaterialItem<number, LocationType>, player?: number): string[] => [];
+export const hideToTOthersWhenRotatedFaceDown: HidingSecretsStrategy<number, LocationType> = (item: MaterialItem<number, LocationType>, player?: number): string[] => {
+    if (item.location.rotation !== KitsuCardRotation.FaceDown)
+    {
+        return [];
+    }
+    return player !== item.location.player ? ['id'] : [];
+}
