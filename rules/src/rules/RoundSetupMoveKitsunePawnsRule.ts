2ed86fe2737ed980f72e94950ad76fc6eb7f55fa
Thibault HUTTIN-PASSERON <thibault.passeron@swhawk.eu>
2025-02-16 21:44:51 +0100

    [44] Feat: Implemented move discarded power tokens to spots on wisdom board

--- rules/src/rules/RoundSetupMoveKitsunePawnsRule.ts
+++ rules/src/rules/RoundSetupMoveKitsunePawnsRule.ts
@@ -5,12 +5,26 @@ import { RuleId } from './RuleId';
 
 export class RoundSetupMoveKitsunePawnsRule extends PlayerTurnRule<number, MaterialType, LocationType> {
     onRuleStart(_move: RuleMove<number, RuleId>, _previousRule?: RuleStep, _context?: PlayMoveContext): MaterialMove<number, MaterialType, LocationType>[] {
-        return [
+        const moves: MaterialMove<number, MaterialType, LocationType>[] = [
             this.material(MaterialType.KitsunePawn).moveItemsAtOnce({
-                type: LocationType.KitsunePawnSpotOnWisdomBoard,
-                id: 0
-            }),
-            this.startRule<RuleId>(RuleId.RoundSetupDealCards),
-        ];
+            type: LocationType.KitsunePawnSpotOnWisdomBoard,
+            id: 0
+        })];
+        moves.push(...this.getPowerTokenMoves());
+        moves.push(this.startRule<RuleId>(RuleId.RoundSetupDealCards))
+        return moves;
+    }
+
+    private getPowerTokenMoves(): MaterialMove<number, MaterialType, LocationType>[] {
+        const discardedPowerTokens = this.material(MaterialType.PowerToken).location(LocationType.DiscardedPowerTokenAreaOnWisdomBoard);
+        if (discardedPowerTokens.length > 0)
+        {
+            return [
+                discardedPowerTokens.moveItemsAtOnce({
+                    type: LocationType.PowerTokenSpotOnWisdomBoard
+                })
+            ];
+        }
+        return [];
     }
 }
