d4d1823ab47711fe6325d5d047b4af1c40a4d062
Thibault HUTTIN-PASSERON <thibault.passeron@swhawk.eu>
2025-03-06 10:15:29 +0100

    [49] Feat: added Leader token help dialog

--- app/src/material/LeaderTokenDescription.ts
+++ app/src/material/LeaderTokenDescription.ts
@@ -1,12 +1,14 @@
 import { LocationType } from '@gamepark/kitsu/material/LocationType';
 import { MaterialType } from '@gamepark/kitsu/material/MaterialType';
 import { TokenDescription } from '@gamepark/react-game';
+import { LeaderTokenHelp } from '../components/Help/LeaderTokenHelp';
 import FirstPlayerToken from '../images/Tokens/FirstPlayerToken.png';
 
 class LeaderTokenDescription extends TokenDescription<number, MaterialType, LocationType> {
     height = 4.00;
     width = 3.50;
     image = FirstPlayerToken;
+    help = LeaderTokenHelp
 }
 
 export const leaderTokenDescription = new LeaderTokenDescription();
