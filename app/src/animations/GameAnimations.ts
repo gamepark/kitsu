6882fe045345f25fff9cd8e99244a491336fa2c9
Thibault HUTTIN-PASSERON <thibault.passeron@swhawk.eu>
2025-03-12 10:35:27 +0100

    [69] Fix: fixed translation and adapted animation speed

--- app/src/animations/GameAnimations.ts
+++ app/src/animations/GameAnimations.ts
@@ -1,3 +1,13 @@
+import { LocationType } from '@gamepark/kitsu/material/LocationType'
+import { MaterialType } from '@gamepark/kitsu/material/MaterialType'
+import { RuleId } from '@gamepark/kitsu/rules/RuleId'
 import { MaterialGameAnimations } from '@gamepark/react-game'
+import { isMoveItemType } from '@gamepark/rules-api'
 
 export const gameAnimations = new MaterialGameAnimations()
+
+gameAnimations
+  .when()
+  .rule<RuleId>(RuleId.RoundSetupDealCards)
+  .move((move) => isMoveItemType<number, MaterialType, LocationType>(MaterialType.KitsuCard)(move) && move.location.type === LocationType.PlayerHand)
+  .duration(0.5)
