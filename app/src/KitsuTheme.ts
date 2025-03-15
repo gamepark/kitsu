762831b6e9edc8bdf711434e520968a15cb92f82
Thibault HUTTIN-PASSERON <thibault.passeron@swhawk.eu>
2025-03-15 16:45:00 +0100

    [70] Feat: added theme for Kitsu, replaced cover art

---
+++ app/src/KitsuTheme.ts
@@ -0,0 +1,10 @@
+import blackBackground from './images/Backgrounds/BlackBackground.jpg'
+
+export const KitsuTheme = {
+  root: {
+    background: {
+      image: blackBackground,
+      overlay: 'rgba(0, 0, 0, 0.3)',
+    }
+  }
+}
